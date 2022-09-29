8cd57acfe061fdacf4ea8a8004be0c95fbdae286
Alex Berbenitskiy <volgaireland@gmail.com>
2022-09-29 16:06:51 +0100

    Exercise 1 completed

--- src/components/exercises/01_staticComponent.js
+++ src/components/exercises/01_staticComponent.js
@@ -2,8 +2,54 @@ import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
+  const course = "Applied Computing - Modules table";
+  const modules = [
+    {
+      name: "DevOps",
+      noLectures: 2,
+      noPracticals: 3
+    },
+    {
+      name: "Automotive",
+      noLectures: 3,
+      noPracticals: 2
+    },
+    {
+      name: "Web App Dev",
+      noLectures: 3,
+      noPracticals: 2
+    }
+  ];
   return (
-      <h1>TODO</h1>
+    <div>
+      <h2>{course}</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{modules[0].name}</td>
+            <td>{modules[0].noLectures}</td>
+            <td>{modules[0].noPracticals}</td>
+          </tr>
+          <tr>
+            <td>{modules[1].name}</td>
+            <td>{modules[1].noLectures}</td>
+            <td>{modules[1].noPracticals}</td>
+          </tr>
+          <tr>
+            <td>{modules[2].name}</td>
+            <td>{modules[2].noLectures}</td>
+            <td>{modules[2].noPracticals}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
